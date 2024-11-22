9a831783c16f5a1335dbeaff737f9c804048c6e0
Richard Porter <richardporter@propellocloud.com>
2024-11-22 17:22:29 +0000

    added binding of reader facade

--- src/LaravelTranslateServiceProvider.php
+++ src/LaravelTranslateServiceProvider.php
@@ -10,12 +10,20 @@ class LaravelTranslateServiceProvider extends PackageServiceProvider
 {
     public function boot(): PackageServiceProvider
     {
+        $reader = config('translate.reader', 'php');
         $provider = config('translate.provider', 'google');
+
+        $readerClass = config("translate.readers.{$reader}");
         $providerClass = config("translate.providers.{$provider}.package");
 
         $this->app->bind(
             'translate',
             fn () => new ($providerClass)()
+        );
+
+        $this->app->bind(
+            'reader',
+            fn () => new ($readerClass)()
         );
 
         return parent::boot();
