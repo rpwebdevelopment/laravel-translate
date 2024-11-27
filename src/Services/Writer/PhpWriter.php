69b2cbfd68315d9f2824399cd9d2e6169510b5fb
Richard Porter <richardporter@propellocloud.com>
2024-11-27 14:00:40 +0000

    writer improvement

--- src/Services/Writer/PhpWriter.php
+++ src/Services/Writer/PhpWriter.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace RPWebDevelopment\LaravelTranslate\Services\Writer;
 
 use RPWebDevelopment\LaravelTranslate\Contracts\Writer;
+use Symfony\Component\VarExporter\VarExporter;
 
 class PhpWriter extends Writer
 {
 
     protected function writeToFile(string $filepath, array $translations): void
     {
-        $content = "<?php\n\nreturn " . var_export($translations, true) . ';';
+        $content = "<?php\n\nreturn " . VarExporter::export($translations) . ';\n';
 
         file_put_contents($filepath, $content);
     }
